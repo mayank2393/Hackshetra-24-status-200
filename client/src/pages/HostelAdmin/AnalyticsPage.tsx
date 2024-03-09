a20811023d400d67355f553dbd7a514339bf70f1
Mayank Tripathi <mayankchamps27@gmail.com>
2024-03-09 19:39:15 +0530

    server added

--- client/src/pages/HostelAdmin/AnalyticsPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function AnalyticsPage(){
-    return (
-        <div className="w-[98%] min-h-[91svh] bg-[#222831] text-white rounded-lg m-auto">
-            <h1>Analytics Page</h1>
-        </div>
-    )
-}
