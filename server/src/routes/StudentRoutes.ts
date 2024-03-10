cb754c1c0ebe067d565fefc57cd930856a1d2769
Mayank Tripathi <mayankchamps27@gmail.com>
2024-03-10 13:03:45 +0530

    mess controllers done

--- server/src/routes/StudentRoutes.ts
+++ server/src/routes/StudentRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { edit_profile } from "../controllers/Student";
+import { edit_profile, fileMessRebate, getMessRebate } from "../controllers/Student";
 import authenticateToken from "../middlewares/jwtVerification";
 import { studentAuth } from "../middlewares/roleAuthentication";
 import upload from "../utilities/multerInitialize";
 
 const StudentRoutes: Router = Router();
 StudentRoutes.put("/edit_profile", upload.single("file"), authenticateToken, studentAuth , edit_profile);
+StudentRoutes.post("/rebate", authenticateToken, studentAuth , fileMessRebate);
+StudentRoutes.get("/rebate", authenticateToken, studentAuth , getMessRebate);
 
 export default StudentRoutes;
